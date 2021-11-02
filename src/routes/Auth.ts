712ee73584946ec2c32da9cdd9a446498b95e0fd
M4gicalCat <philippefaisandier@gmail.com>
2021-11-02 19:04:50 +0100

    Envoie un mail à l'adresse donnée

--- src/routes/Auth.ts
+++ src/routes/Auth.ts
@@ -1,11 +1,11 @@
 import {Router} from "express";
 import {getRepository} from "typeorm";
 import {User} from "../entity/User";
-import session from "express-session";
-import {createDeflateRaw} from "zlib";
-import {makeWatchHost} from "ts-loader/dist/servicesHost";
+import logger from "node-color-log";
+import * as console from "console";
 const passport = require("passport");
 const bcrypt = require('bcrypt');
+const nodemailer = require('nodemailer');
 //pour hash le mdp
 const saltRounds = 10;
 
@@ -71,6 +71,48 @@ export function Route(router: Router) {
 
         })
     })
+
+    router.get('/auth/getPassword', (req, res) => {
+        res.render("auth/getPassword", {erreur: req.query.erreur});
+    });
+
+    router.post("/auth/envoyerMail", async (req, res) => {
+        let repo = getRepository(User);
+        let user = await repo.find({where: {AdresseMail: req.body.mail}})
+        if (user.length === 0){
+            return res.redirect("/auth/getPassword?erreur = 1")
+        }
+
+        let code = "AXBCD"
+
+        const transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+                user: "the.village.dont.trust.anyone@gmail.com",
+                pass: "theVillage1*"
+            }
+        })
+
+        const mailOptions = {
+            from: "the.village.dont.trust.anyone@gmail.com",
+            to: req.body.mail,
+            subject: "Récupération de mot de passe",
+            text: "voici le code nécessaire à la récupération de votre mot de passe : " + code
+        };
+
+        transporter.sendMail(mailOptions, (err, info) => {
+            if (err){
+                console.log(err)
+                res.redirect("/auth/getPassword")
+            }
+            else if (info) {
+                console.log(`Email sent to ${req.body.mail} : ${info.response}`);
+                return res.redirect("/auth")
+            }
+        })
+    })
+
+
 
     router.post('/auth/loginRequest', passport.authenticate('local', {
         successRedirect: '/',
