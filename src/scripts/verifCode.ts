f27d3ca4e2c7e9aa1b9d053f9a7f0bc615d44c5b
M4gicalCat <philippefaisandier@gmail.com>
2021-11-04 13:12:33 +0100

    Récupération de mot de passe par mail OK

--- src/scripts/verifCode.ts
+++ src/scripts/verifCode.ts
@@ -9,8 +9,8 @@ password.on("input", verif)
 password2.on("input", verif)
 
 function verif(){
-    let string = verifMdp(password[0].value)
-    $("#password_info").text((string === "" ? "Mot de passe fort" : verifMdp(password[0].value)))
+    let string = verifMdp(password.val())
+    $("#password_info").text((string === "" ? "Mot de passe fort" : verifMdp(password.val())))
     password.css("box-shadow", `0 0 3px 3px ` + (string === "" ? "green" : "red"))
-    password2.css("box-shadow", `0 0 3px 3px ` + (password[0].value === password2[0].value ? "green" : "red"))
+    password2.css("box-shadow", `0 0 3px 3px ` + (password.val() === password2.val() ? "green" : "red"))
 }
