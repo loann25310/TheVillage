a573c0e0a19448409c27f169d6137943e39f1850
M4gicalCat <philippefaisandier@gmail.com>
2021-12-20 15:34:38 +0100

    changed the way to know if user is connected (Menu) (oopsi)

--- src/routes/Menu.ts
+++ src/routes/Menu.ts
@@ -7,7 +7,7 @@ const bcrypt = require('bcrypt');
 export function Route(router: Router) {
 
     router.get('/', (req, res) => {
-        res.render('main/menu', {user: (req.user as User).id});
+        res.render('main/menu', {user: (req.user as User)?.id});
 
     });
 
