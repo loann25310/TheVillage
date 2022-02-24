163fa4e1398ee597245b7545667c410db5d7f5f9
ImSlaym <yohann.marchand@hotmail.fr>
2022-02-24 20:38:27 +0100

    Simon Says fini

--- src/entity/Props/House.ts
+++ src/entity/Props/House.ts
@@ -22,23 +22,26 @@ export class House extends Displayable {
         this.jeu = {
             ecran: [],
             compteur: 0,
+            compteurCase:0,
+            compteurTotal: 0,
             maxTemps: null,
-            day: null,
+            date: null,
             mouseX: 0,
             mouseY: 0,
             caseAttendue: null,
             click: false,
-            tromper: false
+            tromper: false,
+            clickable: false
         };
         for (let i = 0; i < 5; i++) {
             this.jeu.ecran.push(Math.floor(Math.random() * 9))
         }
+        this.jeu.date = new Date;
         console.log(this.jeu.ecran);
     }
 
     drawJeu() {
-        this.jeu.day = new Date();
-
+        this.jeu.date = new Date();
         const ctx = this.miniJeuCanvas.getContext("2d");
         ctx.clearRect(0, 0, this.miniJeuCanvas.width, this.miniJeuCanvas.height);
         ctx.beginPath();
@@ -55,30 +58,49 @@ export class House extends Displayable {
         ctx.fillStyle = "#16ef1e";
         if(this.jeu.tromper) {
             ctx.fillStyle = "#fc0000";
-            if(this.jeu.maxTemps < this.jeu.day.getTime()) this.initJeu();
+            if(this.jeu.maxTemps < this.jeu.date.getTime()) this.initJeu();
         }
-        for(let i = 0; i < this.jeu.compteur; i++) {
+        for(let i = 0; i < this.jeu.compteurTotal; i++) {
             ctx.ellipse(i * 100 + 315, 200, 5, 5, Math.PI, 0, 2*Math.PI);
             ctx.fill();
         }
         ctx.fillStyle = "#000000";
         ctx.fillRect(275, 255, 490, 490);
         if(this.jeu.click){
-            this.jeu.maxTemps = null;
             this.jeu.click = false;
-            this.jeu.compteur+=1;
+            if (this.jeu.compteur < this.jeu.compteurTotal) {
+                this.jeu.compteur += 1;
+            } else {
+                this.jeu.compteur = 0;
+                this.jeu.compteurTotal++;
+                this.jeu.clickable = false;
+                this.jeu.compteurCase = 0;
+                this.jeu.maxTemps = this.jeu.date.getTime() + 1000;
+            }
         }
+        this.jeu.caseAttendue = this.jeu.ecran[this.jeu.compteur];
+
         if(this.jeu.maxTemps === null) {
-            this.jeu.maxTemps = this.jeu.day.getTime()+1000;
+            this.jeu.maxTemps = this.jeu.date.getTime()+1000;
+        }
+
+        if(this.jeu.date.getTime() > this.jeu.maxTemps-100) {
+            if (this.jeu.compteurTotal > this.jeu.compteurCase) {
+                this.jeu.compteurCase++;
+                this.jeu.maxTemps = this.jeu.date.getTime() + 1000;
+            } else {
+                this.jeu.clickable = true;
+            }
         }
         for (let i = 0; i < 3; i++) {
             for (let j = 0; j < 3; j++) {
-                if(this.jeu.day.getTime() <= this.jeu.maxTemps) {
-                    this.jeu.caseAttendue = this.jeu.ecran[this.jeu.compteur];
-                    if (this.jeu.ecran[this.jeu.compteur] == (i * 3 + j)) {
+                if(this.jeu.date.getTime() < this.jeu.maxTemps-100) {
+                    if (this.jeu.ecran[this.jeu.compteurCase] == (i * 3 + j)) {
                         ctx.fillStyle = "#16ef1e";
                         ctx.fillRect(j * 150 + 300, i * 150 + 280, 140, 140);
                     }
+                }else {
+                    break;
                 }
             }
         }
@@ -93,11 +115,11 @@ export class House extends Displayable {
             }
         }
 
-        if (this.jeu.compteur === 5) this.emit("end_game", true);
+        if (this.jeu.compteurTotal === 5) this.emit("end_game", true);
     }
 
     handleMouseUp() {
-        if(!this.jeu.tromper) {
+        if(!this.jeu.tromper && this.jeu.clickable) {
             if (this.jeu.mouseY >= Math.floor(this.jeu.caseAttendue / 3) * 150 + 280 &&
                 this.jeu.mouseY <= Math.floor(this.jeu.caseAttendue / 3) * 150 + 280 + 140 &&
                 this.jeu.mouseX >= this.jeu.caseAttendue % 3 * 150 + 925 &&
@@ -106,7 +128,7 @@ export class House extends Displayable {
                 this.jeu.click = true;
             }else{
                 this.jeu.tromper = true;
-                this.jeu.maxTemps = this.jeu.day.getTime()+500;
+                this.jeu.maxTemps = this.jeu.date.getTime()+500;
             }
         }
     }
